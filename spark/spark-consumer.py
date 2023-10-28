8fdea2a288007a9a2941ab44a5c6968b69704402
mertakcay <kcaymer1@gmail.com>
2023-10-28 15:56:03 +0300

    - create dummy spark consumer

---
+++ spark/spark-consumer.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+import os 
+
+KAFKA_BOOTSTRAP_SERVERS = "localhost:9094"
+KAFKA_TOPIC = "examplekafka-stat"
+
+
+spark = SparkSession.builder.appName("read_test_straeam").getOrCreate()
+
+# Reduce logging
+spark.sparkContext.setLogLevel("WARN")
+
+df = spark.readStream.format("kafka") \
+    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS) \
+    .option("subscribe", KAFKA_TOPIC) \
+    .option("startingOffsets", "earliest") \
+    .load()
+
+batch = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+    .writeStream \
+    .format("console") \
+    .outputMode("append") \
+    .start() \
+    .awaitTermination()
+    
+print(batch)
